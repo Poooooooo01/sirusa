ebb5772396806264a7a33e7d27c71bbfeefe98f6
unknown <fikrirahmat0407@gmail.com>
2024-05-27 20:15:25 +0700

    nambahin migrasi

--- database/migrations/2024_05_27_114602_create_doctors_table.php
+++ database/migrations/2024_05_27_114602_create_doctors_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string('nik')->unique();
+            $table->string('name');
+            $table->string('specialization');
+            $table->string('education');
+            $table->string('office_number');
             $table->timestamps();
+            $table->softDeletes(); // This will add the 'deleted_at' column for soft deletes
         });
     }
 
