812a4d71531c9f162b0cd58ae8bf2f30843153ad
muhammadparif <muhammadparif028@gmail.com>
2024-05-30 22:34:35 +0800

    Add Fitur Telemedicine

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -74,12 +74,12 @@
                   <li><a href="#">Deep Drop Down 5</a></li>
                 </ul>
               </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="">Drop Down 2</a></li>
+              <li><a href="">Drop Down 3</a></li>
+              <li><a href="">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="nav-link scrollto" href="contact">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -191,7 +191,7 @@
   <div class="container">
     <div class="row justify-content-center">
 
-     
+
 
       <div class="col-lg-3 col-md-6 mb-4">
         <div class="count-box text-center p-4">
