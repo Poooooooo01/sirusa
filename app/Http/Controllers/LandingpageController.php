ae3ccc169c15cfb3cd3d8e0b2d776d3153e979ff
resha <resha@gmail.com>
2024-05-31 09:45:19 +0700

    modified:   app/Http/Controllers/LandingpageController.php 	modified:   resources/views/dashboard/index.blade.php

--- app/Http/Controllers/LandingpageController.php
+++ app/Http/Controllers/LandingpageController.php
@@ -12,13 +12,12 @@ class LandingpageController extends Controller
     public function index()
     {
 
-        $doctors = Doctor::all();
+
         $doctorsCount = Doctor::count();
         $patientCount = Patient::count();
         $adminCount = User::where('role', 'admin')->orWhere('role', 'superadmin')->count();
 
         return view('dashboard.index', compact('adminCount', 'doctorsCount', 'patientCount'));
-        return view('dashboard.index', compact('doctors'));
         // $data = [
         //     "title" => "Dashboard",
         // ];
